3c2c5f94058bba9f96de0e74bc2a853f3532eb7a
Lomakina EA <elomakina1906@yandex.ru>
2021-04-03 14:20:44 +0300

    Tutorial Part 3 26.30 done: sharing content and using it in html, using for, if, etc. in html, html inheritance

--- first/views.py
+++ first/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
 
+posts = [
+    {
+        'author': 'Author 1',
+        'title': 'Post 1',
+        'date': 'date 1'
+    },
+    {
+        'author': 'Author 2',
+        'title': 'Post 2',
+        'date': 'date 2'
+    }
+]
+
 def home(request):
-    return render(request, 'first/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'first/home.html', context)
 
 def about(request):
-    return render(request, 'first/about.html')
+    return render(request, 'first/about.html', {'title': 'About'})
