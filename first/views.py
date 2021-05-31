ecd70e97efeb383822de84460c08672803780c29
Lomakina EA <elomakina1906@yandex.ru>
2021-05-31 15:26:54 +0300

    Before fixing css, save all

--- first/views.py
+++ first/views.py
@@ -7,7 +7,7 @@ def home(request):
 
 
 def about(request):
-    data = pandas.read_csv('k3_csv_2.csv')
+    data = pandas.read_csv('k4.csv')
     city = request.GET['city'].lower().strip()
     month = request.GET['month'].lower().strip()
     top = request.GET['top']
@@ -21,7 +21,7 @@ def about(request):
             r = rating_city_month(city, month, data)
             if r != 0:
                 rating = {'rating': r}
-                return render(request, 'first/about.html', rating)
+                return render(request, 'first/rating_city_month.html', rating)
             else:
                 rating = {'rating': "В базе данных нет такого города"}
                 return render(request, 'first/error.html', rating)
@@ -30,7 +30,7 @@ def about(request):
             if len(r) != 0:
                 rating = {'rating': rating_city(city, data)}
                 return render(request,
-                              'first/month_to_top_of_cities.html', rating)
+                              'first/for_lists.html', rating)
             else:
                 rating = {'rating': "В базе данных нет такого города"}
                 return render(request, 'first/error.html', rating)
@@ -38,7 +38,7 @@ def about(request):
         if month != "":
             rating = {'rating':
                       month_to_top_of_cities(month, data)[:int(top)]}
-            return render(request, 'first/month_to_top_of_cities.html', rating)
+            return render(request, 'first/for_lists.html', rating)
         else:
             rating = {'rating': "Данные не введены"}
             return render(request, 'first/error.html', rating)
@@ -96,10 +96,10 @@ def rating_city(city, data):
 def month_to_top_of_cities(month, data):
     month_we_need = month[0:len(month) - 1]
     d = {}  # Ключ: город; Значение: list(сумма оценок, количество оценок)
+    i = 0
     for value in data.iloc:
-        month_split = value['дата'].split(' ')
-        month = month_split[3]
-        if month.startswith(month_we_need):
+        i += 1
+        if value['дата'].split(' ')[3].startswith(month_we_need):
             city = value['город']
             if d.get(city) is not None:
                 d[city][0] += value['оценка']  # Увеличиваем сумму оценок
@@ -107,7 +107,8 @@ def month_to_top_of_cities(month, data):
             else:
                 d[city] = [value['оценка'], 1]
     lst = []
+    i = 0
     for x in d:
+        i += 1
         lst.append([x, d[x][0] / d[x][1]])
-    lst = sorted(lst, key=lambda rec: rec[1]*(-1))
-    return lst
+    return sorted(lst, key=lambda rec: rec[1]*(-1))
